eefad3200871f17372ea30aca56fd06ae75e5ed1
YeongJae0114 <yjl8628@naver.com>
2025-05-30 13:19:35 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/toy/devtilor/devtilor/DevTiLorApplication.java
@@ -0,0 +1,13 @@
+package com.toy.devtilor.devtilor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTiLorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevTiLorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/devtilor/devtilor/DevTiLorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.devtilor.devtilor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTiLorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
