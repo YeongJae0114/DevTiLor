ee0919e47058bff3e129ae703fa8f6099ca0f5a2
YeongJae0114 <yjl8628@naver.com>
2025-06-03 01:05:22 +0900

    feature: OAuth2 사용자 응답 모델 및 파서 구현

---
+++ src/main/java/com/toy/devtilor/devtilor/auth/oauth/GoogleResponse.java
@@ -0,0 +1,29 @@
+package com.toy.devtilor.devtilor.auth.oauth;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GoogleResponse implements OAuth2Response{
+    private final Map<String, Object> attribute;
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attribute.get("name").toString();
+    }
+}
